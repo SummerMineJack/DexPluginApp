a6f0575f114e02d4ebdafaf480f079d5a2662d83
HuangJian <hjzxzone@163.com>
2019-03-24 20:48:38 +0800

    1，Sophix无法新增Activity

--- thridapp/src/main/java/com/huangjian/thridapp/MainActivity.java
+++ thridapp/src/main/java/com/huangjian/thridapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.huangjian.thridapp;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.huangjian.appdexplugin.PluginActivity;
 
 public class MainActivity extends PluginActivity {
@@ -10,5 +12,11 @@ public class MainActivity extends PluginActivity {
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btnClickListener).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtils.showLong("来自插件app的Toast提示");
+            }
+        });
     }
 }
